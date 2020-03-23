a7d036baca42a46de3da7914ccebafe7a961299f
przemyslawLupak <przemyslaw.lupak@interia.pl>
2020-03-23 20:05:56 +0100

    Created angular project and completed the feature 1

---
+++ angular-shop/src/app/servises/book.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { Book } from '../common/book';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class BookService {
+
+  private baseURL = "http://localhost:8080/api/v1/books";
+  constructor(private httpClient: HttpClient) { }
+getBooks():Observable<Book[]>{
+  return this.httpClient.get<GetResponceBooks>(this.baseURL).pipe(
+    map(responce=> responce._embedded.books)
+  ); 
+}
+}
+interface GetResponceBooks{
+  _embedded:{
+    books: Book[];
+  }
+}
--- onlinebookstore/src/main/java/com/przemyslawlupak/onlinebookstore/repository/BookRepository.java
+++ onlinebookstore/src/main/java/com/przemyslawlupak/onlinebookstore/repository/BookRepository.java
@@ -1,8 +1,9 @@
 package com.przemyslawlupak.onlinebookstore.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import com.przemyslawlupak.onlinebookstore.entity.Book;
-
+@CrossOrigin("http://localhost:4200")//allow angular request to acess spring data
 public interface BookRepository extends JpaRepository<Book, Long> {
 
 }
